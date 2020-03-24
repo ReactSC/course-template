3b8af01e8446dd4ce11807729ed1e270999c5893
Sofiqul Islam <schoolwab@gmail.com>
2020-03-25 03:15:54 +0600

    Updates

---
+++ precache-manifest.95793c6d1af8d7ac624f19bfed384687.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3ca67e27aa7fc74b2549732283d7b8c",
+    "url": "/course-template/index.html"
+  },
+  {
+    "revision": "650389deaaa25ad2e328",
+    "url": "/course-template/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "6e5ca1a00c5f457713d5",
+    "url": "/course-template/static/css/main.a547deb8.chunk.css"
+  },
+  {
+    "revision": "650389deaaa25ad2e328",
+    "url": "/course-template/static/js/2.1d63cf9c.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/course-template/static/js/2.1d63cf9c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e5ca1a00c5f457713d5",
+    "url": "/course-template/static/js/main.860882ce.chunk.js"
+  },
+  {
+    "revision": "52e2f808ff10b4bdc11d",
+    "url": "/course-template/static/js/runtime-main.386fe5a4.js"
+  }
+]);
